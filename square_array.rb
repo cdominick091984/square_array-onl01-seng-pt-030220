a814ce15913a75192d49a4b8a998a8c456680471
Domenico Cascone <dcascone84@gmail.com>
2020-03-09 01:03:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  new_array = []
+  #new_array = []
   array.each do |number|
-    new_array.push(number ** 2)
+    #new_array.push(number ** 2)
+    number ** 2
   end
-  new_array
+  #new_array
 end
